04dd1616997522120b61f687f8b837d0b8222b4f
jnaoroji <jennynaoroji@gmail.com>
2023-07-05 12:48:18 +1000

    working on updated post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -146,7 +146,7 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/profile');
     return;
   }
   res.render('signup');
